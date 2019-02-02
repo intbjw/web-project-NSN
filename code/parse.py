b11dc5be94d578139fd6d0480ffb2da86fe6fe8f
KevinBruce2018 <45493765+KevinBruce2018@users.noreply.github.com>
2019-02-02 18:18:51 +0800

    Create parse.py

---
+++ code/parse.py
@@ -0,0 +1,20 @@
+#用于解析web目录的一个类
+class Log():
+    def __init__(self,s):
+        self.parseWebLog(s)
+    
+    def parseWebLog(self,s):
+        #按照双引号来分割字符串
+        s = s.split('"')
+        #得到IP和访问日期
+        self.ip = s[0].split()[0]
+        self.date = s[0].split()[-2][1:]
+        #email和用户名字这里不再获取
+        #得到未被进一步分割的首部字段
+        self.header = s[1]
+        #得到状态码和数据长度
+        self.status_code = s[2].split()[0]
+        self.length = s[2].split()[1]
+        #得到用户提出请求时所在的URL和user-agent
+        self.url = s[3]
+        self.user_agent = s[5]
